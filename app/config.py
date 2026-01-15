d4d4d5164ef882cb0a08e66114bf256152c6069c
Dmytro-Moldovanu <dmytro.moldovanu@gmail.com>
2026-01-15 17:23:31 +0100

    Fertige Website

--- app/config.py
+++ app/config.py
@@ -10,8 +10,6 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'default-dev-key')
     UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static', 'uploads')
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB maximale Größe der hochgeladenen Dateien
-    # EmailJS Einstellungen
-    EMAILJS_USER_ID = os.environ.get('EMAILJS_USER_ID', 'HDkfTM23gfmDWw3oG')
 
 
 class DevelopmentConfig(Config):
